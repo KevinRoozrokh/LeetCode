dd52e015c330b8fa1a091cf53c046d79c7294bb5
Kevin Roozrokh <78693650+KevinRoozrokh@users.noreply.github.com>
2022-06-09 08:56:19 -0500

    Easy

---
+++ 206. Reverse Linked List/index.js
@@ -0,0 +1,14 @@
+function reverseList(head) {
+    if (!head || !head.next) return head;
+    let prev = null;
+    let curr = head;
+    while (curr) {
+        let next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev; 
+}
+
+module.exports = reverseList;
